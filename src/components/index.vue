044c23524834f066906197ff05db09a20c8894d0
Thalissa <thalissa@users.noreply.github.com>
2019-07-29 08:33:53 -0500

    Final fix to the memories

--- src/components/index.vue
+++ src/components/index.vue
@@ -10,62 +10,76 @@
       h2 DappleClan Leaders
       .leaders
         .leader
-          img(src="../assets/img/spiritstar-song.png")
+          .img
+            img(src="../assets/img/spiritstar-song.png")
           .name Spiritstar (-song)
           .timeline 07/24/2009 to 09/06/2009
         .leader
-          img(src="../assets/img/spottedstar-storm.png")
+          .img
+            img(src="../assets/img/spottedstar-storm.png")
           .name Spottedstar (-storm)
           .timeline 09/06/2009 to 09/26/2009
         .leader
-          img(src="../assets/img/bramblestar-song.png")
+          .img
+            img(src="../assets/img/bramblestar-song.png")
           .name Bramblestar (-song)
           .timeline 09/26/2009 to ??/??/2009
         .leader
-          img(src="../assets/img/duskstar-wish.png")
+          .img
+            img(src="../assets/img/duskstar-wish.png")
           .name Duskstar (-wish)
           .timeline ??/??/2009 to 03/23/2010
         .leader
-          img(src="../assets/img/sweetstar-song.png")
+          .img
+            img(src="../assets/img/sweetstar-song.png")
           .name Sweetstar (-song)
           .timeline 03/23/2010 to 05/30/2010
         .leader
-          img(src="../assets/img/twisterstar-heart.png")
+          .img
+            img(src="../assets/img/twisterstar-heart.png")
           .name Twisterstar (-heart)
           .timeline 06/06/2010 to 07/??/2010
         .leader
-          img(src="../assets/img/heatherstar-stem.png")
+          .img
+            img(src="../assets/img/heatherstar-stem.png")
           .name Heatherstar (-storm)
           .timeline 07/??/2010 to 09/??/2010
         .leader
-          img(src="../assets/img/spiralingstar-song.png")
+          .img
+            img(src="../assets/img/spiralingstar-song.png")
           .name Spiralingstar (-song)
           .timeline 09/??/2010 to 01/15/2011
         .leader
-          img(src="../assets/img/hazelstar-gem.png")
+          .img
+            img(src="../assets/img/hazelstar-gem.png")
           .name Hazelstar (-gem)
           .timeline 01/15/2011 to 08/??/2011
       
       h2 DappleClan Medicine Cats
       .medicineCats
         .medicineCat
-          img(src="../assets/img/icedew.png")
+          .img
+            img(src="../assets/img/icedew.png")
           .name Icedew
           .timeline 07/24/2009 to 09/26/2009
         .medicineCat
-          img(src="../assets/img/thundercloud.png")
+          .img
+            img(src="../assets/img/thundercloud.png")
           .name Thundercloud
           .timeline 09/26/2009 to ??/??/2009
         .medicineCat
-          img(src="../assets/img/rainshimmer.png")
+          .img
+            img(src="../assets/img/rainshimmer.png")
           .name Rainshimmer
           .timeline ??/??/2009 to ??/??/2010
         .medicineCat
-          img(src="../assets/img/fatesong.png")
+          .img
+            img(src="../assets/img/fatesong.png")
           .name Fatesong
           .timeline ??/??/2010 to ??/??/2011
         .medicineCat
-          img(src="../assets/img/tigerflame.png")
+          .img
+            img(src="../assets/img/tigerflame.png")
           .name Tigerflame
           .timeline ??/??/2011 - 08/??/2011 
 </template>
@@ -104,8 +118,9 @@
     display flex
   
   .leader img, .medicineCat img
-   max-width 100px
-   border-radius 5px
+    text-align center
+    max-width 100px
+    border-radius 5px
    
   @media (max-width: 815px)
     .leader img, .medicineCat img
